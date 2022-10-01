9888281ed0b5610de2cd979268ec8e91c290fc3a
pnovikow <41334556+pnovikow@users.noreply.github.com>
2022-10-01 09:26:29 +0300

    Add new fiture to animation

--- sh/animations.sh
+++ sh/animations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-
+# --------------
+# The bash functions called from the main script contain the basic LED blinking algorithm
+#
+# ---------
 one ()
 {
   for d in "${ctrl_led[@]}"; do 
@@ -37,5 +39,11 @@ three ()
 rand ()
 {
    echo $(( ( RANDOM % 2 ) )) > "${ctrl_led[$(( ( RANDOM % $number_of_leds ) ))]}"
+   sleep $sleep_time
+}
+
+rand_2 ()
+{
+   echo $(( ( RANDOM % 4 ) )) > "${ctrl_led[$(( ( RANDOM % $number_of_leds ) ))]}"
    sleep $sleep_time
 }
